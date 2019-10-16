dd0346c24b6bc96c7b8ebb1e4233fa35298d14cf
SleepLotus <wangpengrobot@126.com>
2019-10-16 22:18:12 +0800

    maven shade plugin research

---
+++ test-shaded/src/main/java/Test.java
@@ -0,0 +1,13 @@
+import com.google.common.base.Preconditions;
+
+/**
+ * Description of the functionality.
+ *
+ * @author SleepLotus
+ * @since 2019-10-16
+ */
+public class Test {
+    public static void main(String[] args) {
+        Preconditions.checkArgument(true, "Flase");
+    }
+}
