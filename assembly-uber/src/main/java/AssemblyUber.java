d8ed90ce56174c72f6be882abf73d13d0b871584
SleepLotus <wangpengrobot@126.com>
2019-10-22 22:31:05 +0800

    assembly uber

---
+++ assembly-uber/src/main/java/AssemblyUber.java
@@ -0,0 +1,11 @@
+/**
+ * Description of the functionality.
+ *
+ * @author SleepLotus
+ * @since 2019-10-16
+ */
+public class AssemblyUber {
+    public static void main(String[] args) {
+        System.out.println("AssemblyUber");
+    }
+}
